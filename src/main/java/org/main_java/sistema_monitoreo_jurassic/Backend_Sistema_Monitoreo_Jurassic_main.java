a57ed52d6aefd96039e9fd3b4b19b67148508b06
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-11-01 22:38:53 +0100

    uml actualización
    clases

--- src/main/java/org/main_java/sistema_monitoreo_jurassic/Backend_Sistema_Monitoreo_Jurassic_main.java
+++ src/main/java/org/main_java/sistema_monitoreo_jurassic/Backend_Sistema_Monitoreo_Jurassic_main.java
@@ -1,14 +1,12 @@
 package org.main_java.sistema_monitoreo_jurassic;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "org.main_java.sistema_monitoreo_jurassic")
-public class Backend_Springboot_Aspectos_main {
+public class Backend_Sistema_Monitoreo_Jurassic_main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Backend_Springboot_Aspectos_main.class, args);
+        SpringApplication.run(Backend_Sistema_Monitoreo_Jurassic_main.class, args);
     }
 }
--- src/test/java/org/main_java/sistema_monitoreo_jurassic/Backend_Sistema_Monitoreo_Jurassic_main.java
+++ src/test/java/org/main_java/sistema_monitoreo_jurassic/Backend_Sistema_Monitoreo_Jurassic_main.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Backend_Springboot_AspectosTests {
+class Backend_Sistema_Monitoreo_Jurassic_main {
 
     @Test
     void contextLoads() {

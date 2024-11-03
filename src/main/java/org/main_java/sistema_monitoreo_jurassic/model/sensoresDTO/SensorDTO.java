5edc4643225ca714abaf5c80957d185822a9da01
Jaime Lopez Diaz <jlopedia@myuax.com>
2024-11-03 02:39:26 +0100

    -

--- src/main/java/org/main_java/sistema_monitoreo_jurassic/model/sensoresDTO/SensorDTO.java
+++ src/main/java/org/main_java/sistema_monitoreo_jurassic/model/sensoresDTO/SensorDTO.java
@@ -3,7 +3,7 @@ package org.main_java.sistema_monitoreo_jurassic.model.sensoresDTO;
 import lombok.Data;
 
 @Data
-public class SensorDTO {
+public abstract class SensorDTO {
     private String id;
     private String tipo;
     private double limiteInferior;

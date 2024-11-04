f790669675f0a6222d35d8885dca509071d7ef01
Jmrodrival04 <jvaldrod@myuax.com>
2024-11-04 21:16:18 +0100

    main1

--- src/main/java/org/main_java/sistema_monitoreo_jurassic/Backend_Sistema_Monitoreo_Jurassic_main.java
+++ src/main/java/org/main_java/sistema_monitoreo_jurassic/Backend_Sistema_Monitoreo_Jurassic_main.java
@@ -1,12 +1,70 @@
 package org.main_java.sistema_monitoreo_jurassic;
 
+import org.main_java.sistema_monitoreo_jurassic.model.islasDTO.*;
+import org.main_java.sistema_monitoreo_jurassic.model.islasDTO.criaderos.CriaderoAcuaticoDTO;
+import org.main_java.sistema_monitoreo_jurassic.model.islasDTO.criaderos.CriaderoTerrestreDTO;
+import org.main_java.sistema_monitoreo_jurassic.model.islasDTO.criaderos.CriaderoVoladoresDTO;
+import org.main_java.sistema_monitoreo_jurassic.service.IslaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Collections;
 
-@SpringBootApplication(scanBasePackages = "org.main_java.sistema_monitoreo_jurassic")
-public class Backend_Sistema_Monitoreo_Jurassic_main {
+@SpringBootApplication
+public class Backend_Sistema_Monitoreo_Jurassic_main implements CommandLineRunner {
+
+    @Autowired
+    private IslaService islaService;
 
     public static void main(String[] args) {
         SpringApplication.run(Backend_Sistema_Monitoreo_Jurassic_main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Crear y registrar cada tipo de isla y criadero
+
+        // Isla Acuática
+        IslaAcuaticaDTO islaAcuaticaDTO = new IslaAcuaticaDTO(
+                "1", "Isla Acuática", 30, 10, new int[10][10], Collections.emptyList()
+        );
+        islaAcuaticaDTO.setPermiteAcuaticos(true);
+        islaService.create(islaAcuaticaDTO).subscribe();
+
+        // Isla Terrestre Aérea
+        IslaTerrestreAereaDTO islaTerrestreAereaDTO = new IslaTerrestreAereaDTO(
+                "2", "Isla Terrestre Aérea", 40, 15, new int[15][15], Collections.emptyList()
+        );
+        islaTerrestreAereaDTO.setPermiteTerrestres(true);
+        islaTerrestreAereaDTO.setPermiteVoladores(true);
+        islaService.create(islaTerrestreAereaDTO).subscribe();
+
+        // Enfermería
+        EnfermeriaDTO enfermeriaDTO = new EnfermeriaDTO(
+                "3", "Enfermería", 20, 10, new int[10][10], Collections.emptyList()
+        );
+        islaService.create(enfermeriaDTO).subscribe();
+
+        // Criadero Acuático
+        CriaderoAcuaticoDTO criaderoAcuaticoDTO = new CriaderoAcuaticoDTO(
+                "4", "Criadero Acuático", 15, 8, new int[8][8], Collections.emptyList()
+        );
+        islaService.create(criaderoAcuaticoDTO).subscribe();
+
+        // Criadero Terrestre
+        CriaderoTerrestreDTO criaderoTerrestreDTO = new CriaderoTerrestreDTO(
+                "5", "Criadero Terrestre", 15, 8, new int[8][8], Collections.emptyList()
+        );
+        islaService.create(criaderoTerrestreDTO).subscribe();
+
+        // Criadero Voladores
+        CriaderoVoladoresDTO criaderoVoladoresDTO = new CriaderoVoladoresDTO(
+                "6", "Criadero Voladores", 15, 8, new int[8][8], Collections.emptyList()
+        );
+        islaService.create(criaderoVoladoresDTO).subscribe();
+
+        // Confirmación de creación
+        System.out.println("Todas las islas y criaderos han sido creados y registrados.");
     }
 }

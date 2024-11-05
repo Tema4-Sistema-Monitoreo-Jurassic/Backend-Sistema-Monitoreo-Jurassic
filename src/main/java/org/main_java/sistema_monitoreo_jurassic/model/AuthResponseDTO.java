afb4e7b89156e9939f4dd9daa01ea61e2fcc4d22
marcos <marcgabe@myuax.com>
2024-11-05 21:55:32 +0100

    Create AuthResponseDTO.java
    Esta clase se encargara de los detalles de retorno ante un loggeo o registro exitoso

---
+++ src/main/java/org/main_java/sistema_monitoreo_jurassic/model/AuthResponseDTO.java
@@ -0,0 +1,14 @@
+package org.main_java.sistema_monitoreo_jurassic.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponseDTO {
+    private String message;
+    private String token;      // Para el token de autenticación
+    private String role;       // Rol del usuario
+}
+

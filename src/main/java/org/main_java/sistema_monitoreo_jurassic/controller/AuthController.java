4596f5962cf27cf66da0f8b2f9732170b1169bd3
marcos <marcgabe@myuax.com>
2024-11-05 22:00:56 +0100

    Create AuthController.java
    Finalmente, implementaremos AuthController para exponer los puntos finales /login y /register, manejando las solicitudes de manera reactiva con respuestas Mono.

---
+++ src/main/java/org/main_java/sistema_monitoreo_jurassic/controller/AuthController.java
@@ -0,0 +1,34 @@
+package org.main_java.sistema_monitoreo_jurassic.controller;
+
+import org.main_java.sistema_monitoreo_jurassic.model.AuthResponseDTO;
+import org.main_java.sistema_monitoreo_jurassic.model.LoginRequestDTO;
+import org.main_java.sistema_monitoreo_jurassic.model.RegisterRequestDTO;
+import org.main_java.sistema_monitoreo_jurassic.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    // Login endpoint
+    @PostMapping("/login")
+    public Mono<AuthResponseDTO> login(@RequestBody LoginRequestDTO loginRequest) {
+        return authService.login(loginRequest);
+    }
+
+    // Register endpoint
+    @PostMapping("/register")
+    public Mono<AuthResponseDTO> register(@RequestBody RegisterRequestDTO registerRequest) {
+        return authService.register(registerRequest);
+    }
+}
+

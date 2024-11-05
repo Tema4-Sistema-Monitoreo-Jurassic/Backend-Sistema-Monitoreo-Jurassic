03045aff10e15d879c011df2fdd54c8119b3390b
marcos <marcgabe@myuax.com>
2024-11-05 21:56:34 +0100

    Create LoginRequestDTO.java
    Como no esta clase se encarga de los detalles mas especificos para poder loggearse (contrasnia y correo --> este como custom del repo de mongo)

---
+++ src/main/java/org/main_java/sistema_monitoreo_jurassic/model/LoginRequestDTO.java
@@ -0,0 +1,11 @@
+package org.main_java.sistema_monitoreo_jurassic.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class LoginRequestDTO {
+    private String correo;
+    private String password;
+}

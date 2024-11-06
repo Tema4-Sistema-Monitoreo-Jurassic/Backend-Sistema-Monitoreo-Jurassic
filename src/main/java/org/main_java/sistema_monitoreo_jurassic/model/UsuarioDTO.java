a7785a8b50de0cbf1ab8f542157bf661d622ef73
marcos <marcgabe@myuax.com>
2024-11-06 01:04:09 +0100

    Chorradas
    imports y espaciado de nada

--- src/main/java/org/main_java/sistema_monitoreo_jurassic/model/UsuarioDTO.java
+++ src/main/java/org/main_java/sistema_monitoreo_jurassic/model/UsuarioDTO.java
@@ -3,11 +3,7 @@ package org.main_java.sistema_monitoreo_jurassic.model;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Field;
 
-import java.time.OffsetDateTime;
 
 @Data
 @Getter

9175b26be95c461332cda9f009a6a44df07740ed
marcos <marcgabe@myuax.com>
2024-11-09 00:15:58 +0100

    Update RolRepository.java
    Metemos metodo persionalizado para el rol que usamos en authservice

--- src/main/java/org/main_java/sistema_monitoreo_jurassic/repos/RolRepository.java
+++ src/main/java/org/main_java/sistema_monitoreo_jurassic/repos/RolRepository.java
@@ -2,6 +2,8 @@ package org.main_java.sistema_monitoreo_jurassic.repos;
 
 import org.main_java.sistema_monitoreo_jurassic.domain.Rol;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface RolRepository extends ReactiveMongoRepository<Rol, String> {
+    Mono<Rol> findByNombre(String nombre);
 }
